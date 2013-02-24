4c3c04b838d9dadec3c59d4a16544928d128a893
OpenShift guest <krzychukula@gmail.com>
2013-02-24 11:16:23 -0500

    better file

--- spec/FileManagerSpec.js
+++ spec/FileManagerSpec.js
@@ -5,6 +5,24 @@ FileManager:true
 describe("Player", function() {
 
   it('shoud run', function () {
-    expect(onInitFs).toBeTruthy();
+    expect(writeAndRead).toBeTruthy();
   })
+  
+  runs(function() {
+      flag = false;
+      value = 0;
+
+      setTimeout(function() {
+        flag = true;
+      }, 500);
+    });
+    
+    waitsFor(function() {
+      value++;
+      return flag;
+    }, "The Value should be incremented", 750);
+    
+    runs(function() {
+      expect(value).toBeGreaterThan(0);
+    });
 });
